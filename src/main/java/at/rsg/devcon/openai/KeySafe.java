14f9c3c5ae36cd3a293f35083cdf5dc040e0ad30
WRSMAN <stefan.mader@r-software.at>
2024-12-31 12:58:28 +0100

    KeySafe with dummy key added

--- src/main/java/at/rsg/devcon/openai/KeySafe.java
+++ src/main/java/at/rsg/devcon/openai/KeySafe.java
@@ -1,7 +1,16 @@
 package at.rsg.devcon.openai;
 
+/**
+ * The KeySafe class serves as a centralized location for storing sensitive
+ * information, such as API keys, used by the application.
+ *
+ * This class ensures that sensitive credentials are separated from other
+ * application logic for better security and maintainability.
+ * In Production use, the key should be read from an environment variable.
+ * Preferably, use Azure OpenAI which needs no API keys.
+ */
 public class KeySafe {
     //add your own OpenAI API Key here
-    private static final String API_KEY = "xxxx";
+    public static final String API_KEY = "xxx";
 
 }
